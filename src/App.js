6d0261db8ad72081646d7ded801fbd2d7069ff2b
Paul Blackwell <paul@paulblackwell.io>
2021-10-02 08:54:29 +0100

    Added jsconfig.json file and sass modules

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TestAtom from './components/atoms/test-atom/test-atom';
 
-function App() {
+const App = () =>  {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <TestAtom />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/atoms/test-atom/test-atom.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './test.module.scss';
+
+const TestAtom = () => {
+    return (
+        <div className={styles.test}>
+            I am a TestAtom
+        </div>
+    )
+}
+
+export default TestAtom;
