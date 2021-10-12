f2c3a81f88d9590e68fd76f3a3533097ec2b1bf0
Paul Blackwell <paul@paulblackwell.io>
2021-10-12 20:11:33 +0100

    Reset my local project and started work on the logo

---
+++ src/components/molecules/logo/logo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from '../logo/logo.module.scss'
+
+const Logo = () => (
+    <div className={styles.logo}>
+        <h4 className={styles.logo__text}>MovieSearch</h4>
+    </div>
+);
+
+export default Logo;
