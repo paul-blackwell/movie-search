9b397ab8a17319bbbe7e9ab8f10f0389c818b21d
Paul Blackwell <paul@paulblackwell.io>
2021-10-13 07:38:28 +0100

    Finished the logo styling

--- src/components/molecules/logo/logo.jsx
+++ src/components/molecules/logo/logo.jsx
@@ -3,6 +3,7 @@ import styles from '../logo/logo.module.scss'
 
 const Logo = () => (
     <div className={styles.logo}>
+        <img src='/images/logos/movie-search-logo.svg' alt="MovieSearch logo" className={styles.logo__image}  />
         <h4 className={styles.logo__text}>MovieSearch</h4>
     </div>
 );
