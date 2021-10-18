66fff97a3161135d175f533d502972bbec242a0a
Paul Blackwell <paul@paulblackwell.io>
2021-10-18 08:28:37 +0100

    Started work on the Input comp

---
+++ src/components/atoms/input/input.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Input = ({ type, placeholder }) => {
+  return <div></div>;
+}
+
+Input.propTypes = {};
+
+export default Input;
