ec184c0cef8805e4e68dd43de448d84945706178
Paul Blackwell <paul@paulblackwell.io>
2021-10-19 08:11:41 +0100

    Started work on the InputWithLabel comp

--- src/components/atoms/input/input.jsx
+++ src/components/atoms/input/input.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Input = ({ type, placeholder }) => {
-  return <div></div>;
+const Input = ({ type, value, placeholder, handleChange }) => {
+  if (type === 'submit') return (
+    <input type={type} value="Submit" />
+  )
+
+  return (
+    <input type={type} value={value} onChange={handleChange}  placeholder={placeholder} />
+  )
 }
 
-Input.propTypes = {};
+Input.propTypes = {
+  type: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  handleChange: PropTypes.func.isRequired,
+};
+
+Input.defaultProps = {
+  placeholder: '',
+  value: '',
+}
 
 export default Input;
