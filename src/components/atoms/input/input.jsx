cfdaecf58ebe0bef35361807db94c794617f0235
Paul Blackwell <paul@paulblackwell.io>
2021-10-19 08:20:21 +0100

    Added some styles to the Input comp

--- src/components/atoms/input/input.jsx
+++ src/components/atoms/input/input.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styles from '../input/input.module.scss';
 
 const Input = ({ type, value, placeholder, handleChange }) => {
   if (type === 'submit') return (
@@ -7,7 +8,7 @@ const Input = ({ type, value, placeholder, handleChange }) => {
   )
 
   return (
-    <input type={type} value={value} onChange={handleChange}  placeholder={placeholder} />
+    <input className={styles.input} type={type} value={value} onChange={handleChange}  placeholder={placeholder} />
   )
 }
 
--- src/components/organisms/search/search.jsx
+++ src/components/organisms/search/search.jsx
@@ -3,19 +3,17 @@ import styles from "../search/search.module.scss";
 import { FiSearch } from "react-icons/fi";
 import InputWithLabel from "../../molecules/input-with-label/input-with-label";
 
-
 const Search = () => (
-        <div className={styles.search}>
-            <InputWithLabel 
-            type="text"
-            placeholder="Search by title"
-            value=""
-            handleChange={()=> console.log('I changed')}
-            >
-                <FiSearch className={styles['search__icon']} />
-            </InputWithLabel>
-        </div>
-    );
-
+  <div className={styles.search}>
+    <InputWithLabel
+      type="text"
+      placeholder="Search by title"
+      value=""
+      handleChange={() => console.log("I changed")}
+    >
+      <FiSearch className={styles["search__icon"]} />
+    </InputWithLabel>
+  </div>
+);
 
 export default Search;
