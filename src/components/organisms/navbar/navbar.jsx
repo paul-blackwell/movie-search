613dc35fdb7d2f58f74ae21132f820590d8cca0a
Paul Blackwell <paul@paulblackwell.io>
2021-10-21 16:20:35 +0100

    Removed answer code from unit

--- src/components/organisms/navbar/navbar.jsx
+++ src/components/organisms/navbar/navbar.jsx
@@ -1,23 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import { FiMenu } from "react-icons/fi";
 import styles from "../navbar/navbar.module.scss";
 import Logo from "../../molecules/logo/logo";
 import Nav from "../../molecules/nav/nav";
 
 const Navbar = () => {
-  const [openMobileMenu, setOpenMobileMenu] = useState(false);
-
-  const handleClick = () => {
-    setOpenMobileMenu(!openMobileMenu)
-  }
-
+ 
   return (
     <div className={styles.navbar}>
       <div className={styles["navbar__top-container"]}>
         <Logo />
-        <FiMenu className={styles["navbar__menu-icon"]} onClick={handleClick} />
+        <FiMenu className={styles["navbar__menu-icon"]} onClick={() => console.log('I was clicked')} />
       </div>
-      <div className={`${styles["navbar__bottom-container"]} ${openMobileMenu ? styles["navbar__bottom-container--show"] : styles["navbar__bottom-container--hide"]}`}>
+      <div className={`${styles["navbar__bottom-container"]}`}>
         <Nav />
       </div>
     </div>
