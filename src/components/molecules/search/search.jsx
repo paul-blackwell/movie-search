9deda2729a10ca5dd7a07f3c69af1aced5c07310
Paul Blackwell <paul@paulblackwell.io>
2021-10-25 07:58:29 +0100

    Still working on the mobile fixes to the Header, Search and Nav

--- src/components/molecules/search/search.jsx
+++ src/components/molecules/search/search.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "../search/search.module.scss";
 import { FiSearch } from "react-icons/fi";
 import InputWithLabel from "../input-with-label/input-with-label";
 import SelectWithLabel from "../select-with-label/select-with-label";
 import ButtonPrimary from "../../atoms/button-primary/button-primary";
 
-const Search = () => (
-  <div className={styles.search}>
+const Search = ({className}) => (
+  <div className={`${styles.search} ${className}`}>
     <InputWithLabel
       type="text"
       placeholder="Search by title"
@@ -28,5 +29,14 @@ const Search = () => (
     </div>
   </div>
 );
+
+Search.propTypes = {
+  className: PropTypes.string,
+};
+
+
+Search.defaultProps = {
+  className: '',
+};
 
 export default Search;
