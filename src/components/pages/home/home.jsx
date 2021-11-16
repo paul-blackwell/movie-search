ba834b9e5cd8486c3bb6db49f6f3b3e6284fb6db
Paul Blackwell <paul@paulblackwell.io>
2021-11-16 21:16:38 +0000

    Started work on the Home and PrimaryHeading comp

---
+++ src/components/pages/home/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import defaultMovies from '../../../data/default-movies';
+// import styles from './home.module.scss';
+
+console.log(defaultMovies);
+
+const Home = () => (
+  <div>
+    I am the home
+  </div>
+);
+
+export default Home;
