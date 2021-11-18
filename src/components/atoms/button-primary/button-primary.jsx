f12d5f9b6327822ed60f47957ae5ef8141e8f4f5
Paul Blackwell <paul@paulblackwell.io>
2021-11-18 07:40:44 +0000

    Made some changes to the ButtonPrimary

--- src/components/atoms/button-primary/button-primary.jsx
+++ src/components/atoms/button-primary/button-primary.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './button-primary.module.scss';
 
-const ButtonPrimary = ({ children, handleClick }) => (
-  <button type="button" onClick={handleClick} className={styles['button-primary']}>{children}</button>
+const ButtonPrimary = ({ children, handleClick, hasIcon }) => (
+  <button
+    type="button"
+    onClick={handleClick}
+    className={`${styles['button-primary']} ${hasIcon ? styles['button-primary--icon'] : ''}`}
+  >
+    {children}
+  </button>
 );
 
 ButtonPrimary.propTypes = {
   children: PropTypes.node.isRequired,
   handleClick: PropTypes.func.isRequired,
+  hasIcon: PropTypes.bool,
+};
+
+ButtonPrimary.defaultProps = {
+  hasIcon: false,
 };
 
 export default ButtonPrimary;
--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { FiPlay } from 'react-icons/fi';
 import defaultMovies from '../../../data/default-movies';
 import PrimaryHeading from '../../atoms/typography/primary-heading/primary-heading';
 import TertiaryHeading from '../../atoms/typography/tertiary-heading/tertiary-heading';
+import ButtonPrimary from '../../atoms/button-primary/button-primary';
 // import styles from './home.module.scss';
 
 console.log(defaultMovies);
@@ -11,6 +13,10 @@ const Home = () => (
     <div>
       <PrimaryHeading>Blade Runner</PrimaryHeading>
       <TertiaryHeading>Action, Sci-Fi, Thriller</TertiaryHeading>
+      <ButtonPrimary>
+        <FiPlay />
+        Watch now
+      </ButtonPrimary>
     </div>
   </div>
 );
