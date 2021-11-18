1b07706c7fa0e5918dab372daba7112f302f8734
Paul Blackwell <paul@paulblackwell.io>
2021-11-18 07:52:49 +0000

    Started working on a better mobile nav

--- src/components/molecules/call-to-action/call-to-action.jsx
+++ src/components/molecules/call-to-action/call-to-action.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './call-to-action.module.scss';
 import ButtonSecondary from '../../atoms/button-secondary/button-secondary';
 
-const CallToAction = () => (
-  <div className={styles['call-to-action']}>
+const CallToAction = ({ className }) => (
+  <div className={`${className} ${styles['call-to-action']}`}>
     <h4 className={styles['call-to-action__heading']}>Sign up today!</h4>
     <p className={styles['call-to-action__para']}>Lorem ipsum dolor si amet</p>
     <ButtonSecondary onClick={() => console.log('CallToAction button was clicked')}>Make an account</ButtonSecondary>
   </div>
 );
+
+CallToAction.propTypes = {
+  className: PropTypes.string,
+};
+
+CallToAction.defaultProps = {
+  className: '',
+};
 
 export default CallToAction;
