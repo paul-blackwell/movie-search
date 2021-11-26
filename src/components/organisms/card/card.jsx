c14e77d64d6f233424e51dbbcdcc32ef363180bc
Paul Blackwell <paul@paulblackwell.io>
2021-11-26 11:38:54 +0000

    Finished Card comp styling

--- src/components/organisms/card/card.jsx
+++ src/components/organisms/card/card.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './card.module.scss';
 import StandardImage from '../../atoms/standard-image/standard-image';
-// import MovieScore from '../../atoms/movie-score/movie-score';
-// import SecondaryHeading from '../../atoms/typography/secondary-heading/secondary-heading';
+import MovieScore from '../../atoms/movie-score/movie-score';
+import SecondaryHeading from '../../atoms/typography/secondary-heading/secondary-heading';
+import Paragraph from '../../atoms/typography/paragraph/paragraph';
 
 const Card = ({ className, movie }) => {
-  const { Title, Poster } = movie;
+  const { Title, Poster, Genre } = movie;
 
   return (
     <div className={`${className} ${styles.card}`}>
-      <StandardImage src={Poster} alt={Title} />
+      <div className={styles['card__image-container']}>
+        <StandardImage src={Poster} alt={Title} />
+      </div>
+      <MovieScore score="90%" />
+      <SecondaryHeading>{Title}</SecondaryHeading>
+      <Paragraph>{Genre}</Paragraph>
     </div>
   );
 };
