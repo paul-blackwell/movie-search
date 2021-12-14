e5c424b4d38c72cb0a3cea722d0cf0567bbbcfc5
Paul <paul@paulblackwell.io>
2021-12-14 21:53:50 +0000

    Started work on the PopularSection pagination

--- src/components/organisms/popular-section/popular-section.jsx
+++ src/components/organisms/popular-section/popular-section.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './popular-section.module.scss';
 import SecondaryHeading from '../../atoms/typography/secondary-heading/secondary-heading';
 import PaginationButtons from '../../molecules/pagination-buttons/pagination-buttons';
 import Card from '../../molecules/card/card';
-import useWindowDimensions from '../../../hooks/use-window-dimensions';
+// import useWindowDimensions from '../../../hooks/use-window-dimensions';
 
 const PopularSection = ({ className, movies }) => {
   // This will give us the width of the viewport every time the window size changes
-  const { width } = useWindowDimensions();
-  console.log(width);
+  // const { width } = useWindowDimensions();
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(movies.popular.length);
+
+  // let cardsToShow = movies.popular.length;
+  // if (width > 768) {
+  //   cardsToShow = 2;
+  // }
+  // if (width > 1040) {
+  //   cardsToShow = 3;
+  // }
+  // if (width > 1280) {
+  //   cardsToShow = 4;
+  // }
+
+  const next = () => {
+    if (endIndex > movies.popular.length) {
+      setStartIndex(startIndex + 1);
+      setEndIndex(endIndex + 1);
+    }
+  };
+
+  console.log(cardsToShow);
 
   return (
     <section className={`${styles['popular-section']} ${className}`}>
@@ -20,10 +41,9 @@ const PopularSection = ({ className, movies }) => {
         </div>
       </div>
       <div className={styles['popular-section__cards']}>
-        <Card movie={movies.popular[0]} />
-        <Card movie={movies.popular[1]} />
-        <Card movie={movies.popular[2]} />
-        <Card movie={movies.popular[3]} />
+        {paginatedMovies.map((movie) => (
+          <Card movie={movie} />
+        ))}
       </div>
     </section>
   );
