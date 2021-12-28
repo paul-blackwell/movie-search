b16ab2a9416f539b7f388e3e3a61f5ee1d55f60e
Paul <paul@paulblackwell.io>
2021-12-28 12:40:52 +0000

    Moved the Redux store from app.js to index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import './styles/main.scss';
-import { Provider } from 'react-redux';
-import { store } from './store';
 import Layout from './components/templates/layout/layout';
 import Home from './components/pages/home/home';
 
 const App = () => (
   <div className="App">
     <header className="App-header">
-      <Provider store={store}>
-        <Layout>
-          <Home />
-        </Layout>
-      </Provider>
+      <Layout>
+        <Home />
+      </Layout>
     </header>
   </div>
 );
