cc0650ec3c48cd26e36fcba5d8759029c2404277
paul-blackwell <paul@paulblackwell.io>
2022-01-05 07:32:12 +0000

    Set up react router

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -4,8 +4,6 @@ import HeroSection from '../../organisms/hero-section/hero-section';
 import PopularSection from '../../organisms/popular-section/popular-section';
 import styles from './home.module.scss';
 
-console.log(defaultMovies);
-
 const Home = () => (
   <div className={styles.home}>
     <HeroSection className={styles.home__hero} />
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from './store';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
