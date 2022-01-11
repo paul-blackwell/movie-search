42e53e51761c649e5d551a3b9c96ee40166e98d5
paul-blackwell <paul@paulblackwell.io>
2022-01-11 08:27:36 +0000

    Added the title and genre section to the Movie page comp

--- src/components/pages/movie/movie.jsx
+++ src/components/pages/movie/movie.jsx
@@ -2,9 +2,13 @@ import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import styles from './movie.module.scss';
+import getMovieScore from '../../../utils/getMovieScore';
 import ButtonBack from '../../atoms/button-back/button-back';
 import StandardImage from '../../atoms/standard-image/standard-image';
 import ButtonFavorites from '../../atoms/button-favorites/button-favorites';
+import PrimaryHeading from '../../atoms/typography/primary-heading/primary-heading';
+import Paragraph from '../../atoms/typography/paragraph/paragraph';
+import MovieScore from '../../atoms/movie-score/movie-score';
 
 const Movie = () => {
   // Check if there is a currentlySelectedMovie, if there isn't navigate to home page
@@ -25,12 +29,19 @@ const Movie = () => {
     <div className={styles.movie}>
       <ButtonBack to="/">Back</ButtonBack>
       {movieObj !== '' && (
-      <div className={styles['movie__image-container']}>
-        <div className={styles['movie__button-favorites-container']}>
-          <ButtonFavorites onClick={handleAddToFavorites} />
-        </div>
-        <StandardImage src={movieObj.Poster} alt={movieObj.Title} />
-      </div>
+        <>
+          <div className={styles['movie__image-container']}>
+            <div className={styles['movie__button-favorites-container']}>
+              <ButtonFavorites onClick={handleAddToFavorites} />
+            </div>
+            <StandardImage src={movieObj.Poster} alt={movieObj.Title} />
+          </div>
+          <PrimaryHeading>{movieObj.Title}</PrimaryHeading>
+          <div className={styles['movie__genre-container']}>
+            <Paragraph>{movieObj.Genre}</Paragraph>
+            <MovieScore className={styles.movie__score} score={getMovieScore(movieObj.Ratings)} />
+          </div>
+        </>
       )}
     </div>
   );
