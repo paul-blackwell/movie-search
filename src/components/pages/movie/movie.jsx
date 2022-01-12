ace5676a068965b59baf7539a437c98d66d0ebd5
paul-blackwell <paul@paulblackwell.io>
2022-01-12 07:49:34 +0000

    Mobile styles on Movie comp are now finished

--- src/components/pages/movie/movie.jsx
+++ src/components/pages/movie/movie.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { FiPlay } from 'react-icons/fi';
 import styles from './movie.module.scss';
 import getMovieScore from '../../../utils/getMovieScore';
 import ButtonBack from '../../atoms/button-back/button-back';
@@ -9,6 +10,7 @@ import ButtonFavorites from '../../atoms/button-favorites/button-favorites';
 import PrimaryHeading from '../../atoms/typography/primary-heading/primary-heading';
 import Paragraph from '../../atoms/typography/paragraph/paragraph';
 import MovieScore from '../../atoms/movie-score/movie-score';
+import ButtonPrimary from '../../atoms/button-primary/button-primary';
 
 const Movie = () => {
   // Check if there is a currentlySelectedMovie, if there isn't navigate to home page
@@ -19,6 +21,10 @@ const Movie = () => {
       navigate('/');
     }
   }, []);
+
+  const {
+    Poster, Title, Genre, Ratings, Plot,
+  } = movieObj;
 
   // TODO: Add to favorites
   const handleAddToFavorites = () => {
@@ -34,13 +40,24 @@ const Movie = () => {
             <div className={styles['movie__button-favorites-container']}>
               <ButtonFavorites onClick={handleAddToFavorites} />
             </div>
-            <StandardImage src={movieObj.Poster} alt={movieObj.Title} />
+            <StandardImage src={Poster} alt={Title} />
           </div>
-          <PrimaryHeading>{movieObj.Title}</PrimaryHeading>
+          <PrimaryHeading>{Title}</PrimaryHeading>
           <div className={styles['movie__genre-container']}>
-            <Paragraph>{movieObj.Genre}</Paragraph>
-            <MovieScore className={styles.movie__score} score={getMovieScore(movieObj.Ratings)} />
+            <Paragraph>{Genre}</Paragraph>
+            <MovieScore className={styles.movie__score} score={getMovieScore(Ratings)} />
           </div>
+          <div className={styles['movie__para-container']}>
+            <Paragraph>{movieObj.Plot}</Paragraph>
+          </div>
+          <ButtonPrimary
+            icon={<FiPlay className={styles['movie__button-icon']} />}
+            onClick={() => console.log('Watch now was clicked')}
+            fullWidthOnMobile
+          >
+            Watch now
+
+          </ButtonPrimary>
         </>
       )}
     </div>
