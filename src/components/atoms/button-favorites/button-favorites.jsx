06480b739627b19ad8cd5686a35c234c63153092
paul-blackwell <paul@paulblackwell.io>
2022-01-13 08:13:22 +0000

    ButtonFavorites comp can now be changed to a remove from faorites button

--- src/components/atoms/button-favorites/button-favorites.jsx
+++ src/components/atoms/button-favorites/button-favorites.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FiHeart } from 'react-icons/fi';
+import { FiHeart, FiX } from 'react-icons/fi';
 import styles from './button-favorites.module.scss';
 
-const ButtonFavorites = ({ className, onClick, active }) => (
-  <button onClick={onClick} type="button" className={`${className} ${styles['button-favorites']} ${active ? styles['button-favorites--active'] : ''}`}>
-    <FiHeart className={styles['button-favorites__icon']} />
-  </button>
-);
+const ButtonFavorites = ({
+  className, onClick, active, removeFavorite,
+}) => {
+  if (removeFavorite) {
+    return (
+      <button onClick={onClick} type="button" className={`${className} ${styles['button-favorites']} ${styles['button-favorites--remove']}`}>
+        <FiX className={styles['button-favorites__icon']} />
+      </button>
+    );
+  }
+  return (
+    <button onClick={onClick} type="button" className={`${className} ${styles['button-favorites']} ${active ? styles['button-favorites--active'] : ''}`}>
+      <FiHeart className={styles['button-favorites__icon']} />
+    </button>
+  );
+};
 
 ButtonFavorites.propTypes = {
   className: PropTypes.string,
   onClick: PropTypes.func.isRequired,
   active: PropTypes.bool,
+  removeFavorite: PropTypes.bool,
 };
 
 ButtonFavorites.defaultProps = {
   className: '',
   active: false,
+  removeFavorite: false,
 };
 
 export default ButtonFavorites;
