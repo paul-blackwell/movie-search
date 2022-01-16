5d5340a88310286816714de9aaabedecd4404393
paul-blackwell <paul@paulblackwell.io>
2022-01-16 08:51:01 +0000

    Temp commit

--- src/components/organisms/hero-section/hero-section.jsx
+++ src/components/organisms/hero-section/hero-section.jsx
@@ -7,26 +7,32 @@ import TertiaryHeading from '../../atoms/typography/tertiary-heading/tertiary-he
 import ButtonPrimary from '../../atoms/button-primary/button-primary';
 import HeroImage from '../../atoms/hero-image/hero-image';
 
-const HeroSection = () => (
-  <div className={styles['hero-section']}>
-    <div className={styles['hero-section__score-container']}>
-      <MovieScore score="95%" />
+const HeroSection = () => {
+  const handleClick = () => {
+    console.log('I was clicked');
+  };
+
+  return (
+    <div onClick={handleClick} className={styles['hero-section']}>
+      <div className={styles['hero-section__score-container']}>
+        <MovieScore score="95%" />
+      </div>
+      <div className={styles['hero-section__image-container']}>
+        <HeroImage
+          alt="blade runner 2020"
+          src={`${window.location.origin}/images/default-movies/blade-runner-2049.jpg`}
+          innerShadow
+        />
+      </div>
+      <div className={styles['hero-section__content']}>
+        <PrimaryHeading className={styles['hero-section__title']}>Blade Runner</PrimaryHeading>
+        <TertiaryHeading className={styles['hero-section__tertiary-title']}>Action, Sci-Fi, Thriller</TertiaryHeading>
+        <ButtonPrimary className={styles['hero-section__button-primary']} icon={<FiPlay />}>
+          Watch now
+        </ButtonPrimary>
+      </div>
     </div>
-    <div className={styles['hero-section__image-container']}>
-      <HeroImage
-        alt="blade runner 2020"
-        src={`${window.location.origin}/images/default-movies/blade-runner-2049.jpg`}
-        innerShadow
-      />
-    </div>
-    <div className={styles['hero-section__content']}>
-      <PrimaryHeading className={styles['hero-section__title']}>Blade Runner</PrimaryHeading>
-      <TertiaryHeading className={styles['hero-section__tertiary-title']}>Action, Sci-Fi, Thriller</TertiaryHeading>
-      <ButtonPrimary className={styles['hero-section__button-primary']} icon={<FiPlay />}>
-        Watch now
-      </ButtonPrimary>
-    </div>
-  </div>
-);
+  );
+};
 
 export default HeroSection;
