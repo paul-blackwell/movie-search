75f42568bdde219461e93d6a63e4aee39b6b5629
paul-blackwell <paul@paulblackwell.io>
2022-01-16 10:06:39 +0000

    Added the ButtonBack comp to the MovieSection comp

--- src/components/organisms/movie-section/movie-section.jsx
+++ src/components/organisms/movie-section/movie-section.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { FiPlay } from 'react-icons/fi';
 import styles from './movie-section.module.scss';
 import getMovieScore from '../../../utils/getMovieScore';
+import ButtonBack from '../../atoms/button-back/button-back';
 import ButtonFavorites from '../../atoms/button-favorites/button-favorites';
 import ButtonPrimary from '../../atoms/button-primary/button-primary';
 import StandardImage from '../../atoms/standard-image/standard-image';
@@ -27,6 +28,9 @@ const MovieSection = ({ className, movieObj, favorite }) => {
 
   return (
     <div className={styles['movie-section']}>
+      <ButtonBack to="/">
+        Back
+      </ButtonBack>
       <div className={`${className} ${styles['movie-section__container']}`}>
         <div className={styles['movie-section__image-container']}>
           {!favorite && (
