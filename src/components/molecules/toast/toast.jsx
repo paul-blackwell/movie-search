117c00a4b486b50d6a1ef94ebd588f85307fd851
paul-blackwell <paul@paulblackwell.io>
2022-01-19 07:52:53 +0000

    Added the styles for all the Toast states

--- src/components/molecules/toast/toast.jsx
+++ src/components/molecules/toast/toast.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
-import { FiCheck, FiX } from 'react-icons/fi';
+import PropTypes from 'prop-types';
+import { FiCheck, FiX, FiAlertOctagon } from 'react-icons/fi';
 import styles from './toast.module.scss';
 
-const Toast = () => {
+const Toast = ({ className, error, success }) => {
   const handleClick = () => {
 
   };
 
+  if (error) {
+    return (
+      <div className={`${className} ${styles.toast} ${styles['toast--error']}`}>
+        <div className={styles.toast__container}>
+          <FiCheck className={styles.toast__icon} />
+          <p className={styles.toast__para}>Added Blade Runner to your favorites.</p>
+          <FiAlertOctagon className={styles.toast__icon} />
+        </div>
+      </div>
+    );
+  }
+
+  if (success) {
+    return (
+      <div className={`${className} ${styles.toast} ${styles['toast--success']}`}>
+        <div className={styles.toast__container}>
+          <FiCheck className={styles.toast__icon} />
+          <p className={styles.toast__para}>Added Blade Runner to your favorites.</p>
+          <FiX className={styles.toast__icon} />
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className={styles.toast}>
+    <div className={`${className} ${styles.toast}`}>
       <div className={styles.toast__container}>
         <FiCheck className={styles.toast__icon} />
         <p className={styles.toast__para}>Added Blade Runner to your favorites.</p>
@@ -16,6 +41,18 @@ const Toast = () => {
       </div>
     </div>
   );
+};
+
+Toast.propTypes = {
+  className: PropTypes.string,
+  error: PropTypes.bool,
+  success: PropTypes.bool,
+};
+
+Toast.defaultProps = {
+  className: '',
+  error: false,
+  success: false,
 };
 
 export default Toast;
