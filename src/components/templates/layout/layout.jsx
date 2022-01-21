17be1fce543d8a57e78a3dce2532d8304f563755
paul-blackwell <paul@paulblackwell.io>
2022-01-21 07:34:14 +0000

    The Toast state is now linked to the toast store

--- src/components/templates/layout/layout.jsx
+++ src/components/templates/layout/layout.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import styles from './layout.module.scss';
 import Header from '../../organisms/header/header';
 import Search from '../../organisms/search/search';
@@ -8,6 +9,13 @@ import Toast from '../../molecules/toast/toast';
 
 const Layout = ({ children }) => {
   const [showMobileNav, setShowMobileNav] = useState(false);
+  const [toastState, setToastState] = useState('');
+
+  const toastStore = useSelector((state) => state.toast);
+  console.log(toastStore);
+  useEffect(() => {
+    setToastState(toastStore);
+  }, [toastStore]);
 
   return (
     <div className={styles.layout}>
@@ -23,7 +31,13 @@ const Layout = ({ children }) => {
           {children}
         </div>
       </main>
-      <Toast />
+      <Toast
+        show={toastState.display}
+        hide={toastState.display}
+        message={toastStore.message}
+        error={toastStore.type === 'error'}
+        success={toastStore.type === 'success'}
+      />
     </div>
   );
 };
